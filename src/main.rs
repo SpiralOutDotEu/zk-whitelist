b7ae4d215a1dbeded05efdeb3c12320906f65e5c
Jumaru <spiralout.eu@gmail.com>
2023-10-22 23:24:17 +0300

    feat: :sparkles: Generate the zokrates program file
    Add code that writes the pre-defined zokrates program to `whitelist.zok` file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self,  Write};
+use std::path::Path;
+use std::fs::File;
+
+fn main() -> io::Result<()> {
+    // Check if `whitelist.zok` exists, if not create it
+    if !Path::new("whitelist.zok").exists() {
+        let contents = r#"
+import "hashes/sha256/512bitPacked" as sha256packed;
+
+def main(private field a, private field b, public field c, public field d) -> bool{
+    assert(a == c);
+    assert(b == d);
+    return true;
+}
+"#;
+        File::create("whitelist.zok")?.write_all(contents.as_bytes())?;
+    }
+    Ok(())
 }
