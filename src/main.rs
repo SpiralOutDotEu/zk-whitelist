47c8d6164b99f88561c498d9436f8c83cede3d2f
Jumaru <spiralout.eu@gmail.com>
2023-10-23 20:00:10 +0300

    feat: :sparkles: Implement a pipeline that reads a list of addresses and generates an address-proof file
    This commit introduces several changes aimed at implementing serialization and deserialization features for proof handling.
    
    The key changes include:
    1. Added new dependencies:
     - `serde`
      - `serde_json`
      - `serde_derive`
      - `num_bigint`
      - `num_traits`
    
      2. Created new structs `Proof` and `ProofFile` to represent the proof data structure and its associated file format. These structs are equipped with `Serialize` and `Deserialize` traits for easy serialization and deserialization to/from JSON.
    
      3. Implemented a new function `parse_proof_and_input` that reads a `proof.json` file, deserializes its content into a `ProofFile` instance, and returns the `Proof` and inputs encapsulated within.
    
      4. Extended the `main` function to include logic for:
      - Reading addresses from an `addresses.txt` file.
      - Computing witnesses and generating proofs using external commands.
      - Parsing the generated proofs and inputs from `proof.json`.
      - Aggregating all data into a hashmap.
      - Writing the aggregated data into a new `address-proof.json` file.  This implementation provides the necessary infrastructure for handling proofs in the context of the given application, allowing for streamlined processing and storage of proof-related data.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,44 @@
-use std::fs::File;
-use std::io::{self, Write};
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs::{File, OpenOptions};
+use std::io::{self, BufRead, BufReader, Write};
 use std::path::Path;
 use std::process::Command;
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+extern crate num_bigint;
+extern crate num_traits;
+
+use num_bigint::BigUint;
+use num_traits::Num;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Proof {
+    a: Vec<String>,
+    b: Vec<Vec<String>>,
+    c: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ProofFile {
+    scheme: String,
+    curve: String,
+    proof: Proof,
+    inputs: Vec<String>,
+}
+
+fn parse_proof_and_input() -> Result<(Proof, Vec<String>), Box<dyn Error>> {
+    // Open the proof.json file
+    let file = File::open("proof.json")?;
+    let reader = BufReader::new(file);
+
+    // Deserialize the JSON data
+    let proof_file: ProofFile = serde_json::from_reader(reader)?;
+
+    Ok((proof_file.proof, proof_file.inputs))
+}
 
 fn main() -> io::Result<()> {
     // Check if `whitelist.zok` exists, if not create it
@@ -33,6 +70,67 @@ def main(private field a, private field b, public field c, public field d) -> bo
     // Export a solidity verifier
     let output = Command::new("zokrates").arg("export-verifier").output()?;
     assert!(output.status.success());
+
+    // Open the addresses.txt file
+    let file = File::open("addresses.txt")?;
+    let reader = io::BufReader::new(file);
+
+    // Create a hashmap to hold all the data
+    let mut all_data: HashMap<String, serde_json::Value> = HashMap::new();
+
+    // Loop through each line in addresses.txt
+    for line in reader.lines() {
+        let address = line?;
+        // Remove the "0x" prefix and parse the hex string as a U512
+        let decimal = BigUint::from_str_radix(&address[2..], 16).unwrap();
+        let decimal_str = decimal.to_string();
+        let mid = decimal_str.len() / 2;
+        let (a, b) = decimal_str.split_at(mid);
+        let (c, d) = (a.to_string(), b.to_string());
+
+        // Compute witness
+        let _output = Command::new("zokrates")
+        .arg("compute-witness")
+        .arg("-a")
+        .args(&[&a.to_string(), &b.to_string(), &c.to_string(), &d.to_string()])
+        .output()?;
+        // assert!(output.status.success(), "compute witness failed");
+
+          // Generate the proofs
+        let output = Command::new("zokrates").arg("generate-proof").output()?;
+        assert!(output.status.success());
+
+        // Parse the proof and input from proof.json
+        match parse_proof_and_input() {
+            Ok((proof, input)) => {
+                all_data.insert(
+                    address.clone(),
+                    serde_json::json!({
+                        "proof": [
+                            proof.a,
+                            proof.b,
+                            proof.c
+                        ],
+                        "inputs": input
+                    }),
+                );
+            }
+            Err(e) => eprintln!("Failed to parse proof and input: {}", e),
+        }
+    }
+
+    // Open the address-proof.json file for writing
+    let file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open("address-proof.json")?;
+    let mut writer = io::BufWriter::new(file);
+
+    // Serialize the hashmap to a formatted JSON string
+    let json_string = serde_json::to_string_pretty(&all_data)?;
+
+    // Write the JSON string to the file
+    writer.write_all(json_string.as_bytes())?;
 
     Ok(())
 }
